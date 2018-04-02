4474d7852b268893998689c12917b858bf213c43
wojkot <wojciech.kot93@gmail.com>
2018-04-02 14:44:59 +0200

    Update discovers.js

--- routes/discovers.js
+++ routes/discovers.js
@@ -24,6 +24,7 @@ exports.markSafeDiscovered = async function markSafeDiscovered(req, res) {
     }
     catch (err) {
         console.log(err);
+        res.status(500).send('Something broke!')
     }
 
 };
